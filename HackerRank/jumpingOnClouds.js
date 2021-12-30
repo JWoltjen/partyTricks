453dad27dea22925a76e7912fc97c65b46cb084e
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-12-30 12:33:54 -0600

    solve jumping on clouds with explanation

--- HackerRank/jumpingOnClouds.js
+++ HackerRank/jumpingOnClouds.js
@@ -6,6 +6,26 @@ For each game, you will get an array of clouds numbered 0 if they are safe or 1 
 */
 
 function jumpingOnClouds(c) {
-    // Write your code here
+   
+    //set global variables
+    //Because we're using a do while loop, we keep track of the index externally.
+    let jumps = 0; 
+    let ind = 0; 
+
+        do{
+            jumps++; 
+            //we know we have to jump, the question is how far
+
+            ind = c[ind + 2] == 0 ? ind + 2 : ind + 1
+
+            //a ternary operator lets us look into the future of the array: is index + 2 safe? If yes, increment the index by 2, if it's not safe, only increment the index by 1. 
+
+        } while( ind < c.length -1); 
+        //the while portion sets the scope of the loop. 
+        //as long as ind is less than the clouds in the array, perform
+        //a jump
+    return jumps
 
 }
+
+
