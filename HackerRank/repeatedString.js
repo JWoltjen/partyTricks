c5d79023c27416e94bdafec80ad6f11cac0346d7
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-12-30 13:24:11 -0600

    create repeatedStrings file and pseudocode what is asked for

---
+++ HackerRank/repeatedString.js
@@ -0,0 +1,15 @@
+/*There is a string, , of lowercase English letters that is repeated infinitely many times. Given an integer, n, find and print the number of letter a's in the first n letters of the infinite string.*/
+
+
+function repeatedStrings(s, n) {
+    //declare global variables
+    let count = 0; 
+
+    //count the number of times the letter a appears in a given s
+    //multiply that number by how many s's there are in substring n
+    //if there is a remainder to that number, splice the remaining letters to get the most accurate count
+    
+
+    //return what is asked
+    return count
+}
