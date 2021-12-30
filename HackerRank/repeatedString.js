d914f80c9d788ad1860703ee2b1b9485d434d169
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-12-30 13:40:58 -0600

    solve repeated string problem

--- HackerRank/repeatedString.js
+++ HackerRank/repeatedString.js
@@ -8,8 +8,27 @@ function repeatedStrings(s, n) {
     //count the number of times the letter a appears in a given s
     //multiply that number by how many s's there are in substring n
     //if there is a remainder to that number, splice the remaining letters to get the most accurate count
-    
+
+
+    //first part - how many times is s repeated in n completely?
+    if(n >= s.length){
+        let occurencesOfA = [...s].filter(e => e === 'a').length;
+        count = Math.floor(n/s.length) * occurencesOfA; 
+    }
+
+    //second part - if there are leftovers, take the modulo and clean it up
+    let tailStringLength = n % s.length; 
+    for (let i = 0; i < tailStringLength; i++){
+        if(s[i] === 'a'){
+            count++
+        }
+    }
 
     //return what is asked
     return count
 }
+
+/*
+
+
+*/
